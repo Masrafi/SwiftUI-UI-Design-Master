e29b4a10e3e892d8a603d68022dc747dae2e1fa3
Masrafi <masrafianam@gmail.com>
2024-09-18 16:59:29 +0600

    Initial Commit

---
+++ UI Design/UI_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_DesignApp.swift
+//  UI Design
+//
+//  Created by Md Khorshed Alam on 18/9/24.
+//
+
+import SwiftUI
+
+@main
+struct UI_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
