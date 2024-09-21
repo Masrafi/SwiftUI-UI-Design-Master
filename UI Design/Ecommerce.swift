864536250827b034ed1b1cc05925fa5aa584fac2
Masrafi <masrafianam@gmail.com>
2024-09-21 07:00:31 +0600

    Implement ecommerce ui

---
+++ UI Design/Ecommerce.swift
@@ -0,0 +1,18 @@
+//
+//  Ecommerce.swift
+//  UI Design
+//
+//  Created by Md Khorshed Alam on 20/9/24.
+//
+
+import SwiftUI
+
+struct Ecommerce: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Ecommerce()
+}
